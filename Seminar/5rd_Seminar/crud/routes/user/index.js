aea7b21c9ac2692fdd7cc5555f81e80963445b36
realwhyjay <brandon94_@naver.com>
2021-11-17 15:57:11 +0900

    ⚰️ [DEL] : 쓸모없는 디렉토리 삭제

--- Seminar/5rd_Seminar/crud/routes/user/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// /user/signup
-router.post("/signup", require("./userSignupPOST"));
-router.post("/login", require("./userLoginPOST"));
-router.post("/:id", require("./userUPDATE"));
-
-module.exports = router;
