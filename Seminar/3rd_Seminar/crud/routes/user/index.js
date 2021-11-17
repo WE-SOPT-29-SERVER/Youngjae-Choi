1a600df05f0232b649e881700b413323ae7d27ed
realwhyjay <brandon94_@naver.com>
2021-11-17 16:40:17 +0900

    ✨ [FEAT] : 프로필 조회하기 (Mission 🔥)

--- Seminar/3rd_Seminar/crud/routes/user/index.js
+++ Seminar/3rd_Seminar/crud/routes/user/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 // /user/signup
 router.post("/signup", require("./userSignupPOST"));
 router.post("/login", require("./userLoginPOST"));
+router.get("/profile/:id", require("./userProfileGet"));
 
 module.exports = router;
