ff6aa7606eba75e7259cc0620affb844f1b19bc0
realwhyjay <brandon94_@naver.com>
2021-12-23 11:13:25 +0900

    ✨ [FEAT] : 로그인 기능 구현

--- Seminar/7th_Seminar/functions/api/routes/auth/index.js
+++ Seminar/7th_Seminar/functions/api/routes/auth/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/signup', require('./authSignupPOST'));
-// router.post('/login/email', require('./authLoginPOST'));
+router.post('/login/email', require('./authLoginPOST'));
 
 module.exports = router;
